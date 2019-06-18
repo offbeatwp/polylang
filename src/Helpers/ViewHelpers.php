223d7624194fcb16927179fd9b2bbce7970f66ab
Daan Seegers <daan@raow.nl>
2019-06-18 14:13:04 +0200

    First Commit

---
+++ src/Helpers/ViewHelpers.php
@@ -0,0 +1,10 @@
+<?php
+namespace OffbeatWP\Polylang\Helpers;
+class ViewHelpers {
+
+    public function langSwitcher(){
+
+        return pll_the_languages();
+    }
+
+}
---
+++ src/Service.php
@@ -0,0 +1,12 @@
+<?php
+namespace OffbeatWP\Polylang;
+
+use OffbeatWP\Contracts\View;
+use OffbeatWP\Services\AbstractService;
+
+class Service extends AbstractService {
+    public function register(View $view )
+    {
+        $view->registerGlobal('polylang', new Helpers\ViewHelpers());
+    }
+}
