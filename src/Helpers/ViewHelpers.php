d56a30e706922f8097d170bedd8da22c37f39250
Daan Seegers <daan@raow.nl>
2019-06-20 10:25:34 +0200

    Add new function

--- src/Helpers/ViewHelpers.php
+++ src/Helpers/ViewHelpers.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace OffbeatWP\Polylang\Helpers;
 
 class ViewHelpers
@@ -6,9 +7,20 @@ class ViewHelpers
 
     public function langSwitcher($args = [])
     {
-        if (!function_exists('polylang_langSwitcher')) {
+        if (!function_exists('pll_the_languages')) {
             return __('Polylang langSwitcher does not exists');
         }
         return pll_the_languages($args);
     }
+
+    public function currentLang($field = 'slug')
+    {
+        if (!function_exists('pll_current_language')) {
+            return __('Polylang currentLang does not exists');
+        }
+        return pll_current_language($field);
+
+    }
+
+
 }
