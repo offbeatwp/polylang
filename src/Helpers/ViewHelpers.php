bde0cbeba1c6517159d5932ccb76300fe5d6ba32
Daan Seegers <daan@raow.nl>
2019-06-20 10:08:30 +0200

    Update langSwitcher

--- src/Helpers/ViewHelpers.php
+++ src/Helpers/ViewHelpers.php
@@ -1,11 +1,14 @@
 <?php
 namespace OffbeatWP\Polylang\Helpers;
-class ViewHelpers {
 
-    public function langSwitcher(){
+class ViewHelpers
+{
 
-        return pll_the_languages();
-
+    public function langSwitcher($args = [])
+    {
+        if (!function_exists('polylang_langSwitcher')) {
+            return __('Polylang langSwitcher does not exists');
+        }
+        return pll_the_languages($args);
     }
-
 }
